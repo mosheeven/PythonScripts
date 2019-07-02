a3165d1c9bc872e63887d965fde43c753bfa5ba4
moshe <mosheeve@gmail.com>
2019-07-02 18:02:59 +0300

    Add two functions to create random username and password

--- spam_list.py
+++ spam_list.py
@@ -1,4 +1,27 @@
 import urllib
 import string
 import os
+import random
+import json
+
+
+def randomPassword(name):
+    chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+    passwd = ''.join(random.choice(chars) for i in range(8)) + name
+    return passwd
+
+
+def createRandomEmail(name):
+    name_extra_digit = ''.join(random.choice(string.digits))
+    name_extra_hexdigit = ''.join(random.choice(string.hexdigits))
+    rand_email = name + name_extra_digit + name_extra_hexdigit + '@gmail.com'
+
+    return rand_email
+
+
+names = json.loads(open('spam_list_names.json').read())
+rnd_name = random.choice(names)
+
+print ('Username:' + createRandomEmail(rnd_name))
+print('Password:' + randomPassword(rnd_name))
 
